5f6610eaa59ecbcfbe8c1f00f808ab4af6d07bc2
krrish <krishkadam88@gmail.com>
2025-07-30 16:06:43 +0530

    test agian

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ export function Hero() {
   };
 
   return (
-    <section id="home" className="relative h-screen flex items-center justify-center overflow-hidden">
+    <section id="home" className="relative h-dvh flex items-center justify-center overflow-hidden">
       {/* Background Image */}
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
